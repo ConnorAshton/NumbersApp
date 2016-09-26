194d34d69bb2481a966929a2557094ee4b11d662
ConnorAshton07 <connorashton1995@gmail.com>
2016-09-26 20:53:34 +0100

    First commit

--- Numbers/ViewController.swift
+++ Numbers/ViewController.swift
@@ -10,16 +10,172 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayNumberLbl: UILabel!
+    
+    var runningNumber = ""
+    var leftNumStr = ""
+    var rightNumStr = ""
+    var result = ""
+    var currentOperation = Operation.Empty
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Add = "+"
+        case Subtract = "-"
+        case Empty = "Empty"
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        displayNumberLbl.text = "0"
+    }
+    
+    // Create IBAction for each operator and pass its case into the performCalc function
+
+    @IBAction func numberBtnPressed(_ sender: UIButton) {
+        runningNumber += "\(sender.tag)"
+        displayNumberLbl.text = runningNumber
+    }
+    
+
+    func performCalculation(operation: Operation){
+        
+        // Check that the currentOperation has an operation case already chosen and is not empty.
+        // This is if the user has already pressed a operator then chose another
+        if currentOperation != Operation.Empty {
+            
+            if runningNumber != "" {
+                
+                rightNumStr = runningNumber
+                runningNumber = ""
+                
+            if currentOperation == Operation.Divide {
+                result = "\(Double(leftNumStr)! / Double(rightNumStr)!)"
+            } else if currentOperation == Operation.Multiply {
+                result = "\(Double(leftNumStr)! * Double(rightNumStr)!)"
+            } else if currentOperation == Operation.Add {
+                result = "\(Double(leftNumStr)! + Double(rightNumStr)!)"
+            } else if currentOperation == Operation.Subtract {
+                result = "\(Double(leftNumStr)! - Double(rightNumStr)!)"
+            }
+                leftNumStr = result
+                displayNumberLbl.text = result
+            }
+                
+                currentOperation = operation
+                
+            } else {
+            // This is the first time operator has been pressed
+            leftNumStr = runningNumber
+            runningNumber = ""
+            currentOperation = operation
+        }
+        
+        
+        
+    }
+    
+    @IBAction func dividePressed(sender: AnyObject){
+        performCalculation(operation: .Divide)
+    }
+    
+    @IBAction func multiplyPressed(sender: AnyObject){
+        performCalculation(operation: .Multiply)
+    }
+    
+    @IBAction func addPressed(sender: AnyObject){
+        performCalculation(operation: .Add)
+    }
+    
+    @IBAction func subtractPressed(sender: AnyObject){
+        performCalculation(operation: .Subtract)
+    }
+    
+    @IBAction func equalPressed(sender: AnyObject){
+        performCalculation(operation: currentOperation)
+    }
+    
+    @IBAction func percentPressed(sender: AnyObject){
+        percentButtonPressed()
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clearPressed(sender: AnyObject){
+        runningNumber = ""
+        leftNumStr = ""
+        rightNumStr = ""
+        result = ""
+        currentOperation = Operation.Empty
+        
+        displayNumberLbl.text = "0"
+    }
+    
+    @IBAction func plusMinusPressed() {
+        plusMinusBtnPressed()
+    }
+    
+    @IBAction func decimalPressed(){
+        decimalBtnPressed()
     }
 
+
+    func percentButtonPressed() {
+        
+        // Another operator has been pressed already
+        
+        if currentOperation != Operation.Empty {
+        
+        if runningNumber != "" {
+            
+        rightNumStr = runningNumber
+        
+        rightNumStr = "\(Double(leftNumStr)! * Double(rightNumStr)!)"
+        rightNumStr = "\(Double(rightNumStr)! / 100)"
+            
+        runningNumber = rightNumStr
+            
+        // 100 in LH and now 10 in RH. Press equals
+        
+        }
+        } else {
+            
+            if runningNumber != "" {
+            leftNumStr = runningNumber
+            
+            let result = "\(Double(leftNumStr)! / 100)"
+            runningNumber = result
+            displayNumberLbl.text = result
+                
+            
+            
+            }
+        }
+    }
+    
+    
+    func plusMinusBtnPressed() {
+        
+        if runningNumber != "" {
+        runningNumber = "\(Double(runningNumber)! * -1)"
+        displayNumberLbl.text = runningNumber
+        }
+        
+    }
+    
+    func decimalBtnPressed(){
+        
+            if runningNumber != "" {
+            if runningNumber.range(of: ".") == nil {
+            runningNumber += "."
+            displayNumberLbl.text = runningNumber
+        }
+        
+    }
+    }
+    
 
 }
 
